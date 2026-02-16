fe7b7e7f6ec572c9f3bce199a31e84330c5762cc
cole sashkin <csashceh1024@gmail.com>
2026-02-16 08:57:45 -0800

    update

--- src/main.rs
+++ src/main.rs
@@ -269,7 +269,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let devices = list_usb_devices()?;
         for device in devices {
             println!("VID: 0x{:04X}, PID: 0x{:04X}, Manufacturer: {:?}, Product: {:?}, Serial: {:?}",
-                     device.vendor_id, device.product_id, device.manufacturer, device.product, device.serial_number);
+                     device.vendor_id, device.product_id, device.manufacturer.unwrap(), device.product.unwrap(), device.serial_number);
         }
         return Ok(());
     }
@@ -319,8 +319,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 println!("Found device!");
                 let descriptor = UsbDeviceDescriptor::new(&device)?;
                 println!("Device found: VID: 0x{:04X}, PID: 0x{:04X}", descriptor.vendor_id, descriptor.product_id);
-                println!("Manufacturer: {:?}", descriptor.manufacturer);
-                println!("Product: {:?}", descriptor.product);
+                println!("Manufacturer: {:?}", descriptor.manufacturer.unwrap());
+                println!("Product: {:?}", descriptor.product.unwrap());
                 println!("Serial: {:?}", descriptor.serial_number);
             }
             None => {
@@ -335,8 +335,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 println!("Found device!");
                 let descriptor = UsbDeviceDescriptor::new(&device)?;
                 println!("Device found: VID: 0x{:04X}, PID: 0x{:04X}", descriptor.vendor_id, descriptor.product_id);
-                println!("Manufacturer: {:?}", descriptor.manufacturer);
-                println!("Product: {:?}", descriptor.product);
+                println!("Manufacturer: {:?}", descriptor.manufacturer.unwrap());
+                println!("Product: {:?}", descriptor.product.unwrap());
                 println!("Serial: {:?}", descriptor.serial_number);
             }
             None => {
@@ -349,7 +349,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let devices = list_usb_devices()?;
         for device in devices {
             println!("VID: 0x{:04X}, PID: 0x{:04X}, Manufacturer: {:?}, Product: {:?}, Serial: {:?}",
-                     device.vendor_id, device.product_id, device.manufacturer, device.product, device.serial_number);
+                     device.vendor_id, device.product_id, device.manufacturer.unwrap(), device.product.unwrap(), device.serial_number);
         }
     }
 
